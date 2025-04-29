bd54c68fe86cb1fb95efad96091019b461d2d2b6
MUVUNYI <164413556+Philemon5MUVUNYI@users.noreply.github.com>
2025-04-29 13:19:51 +0200

    Delete Assignment_one directory

--- Assignment_one/ConstructionMaterial.java
+++
@@ -1,9 +0,0 @@
-public abstract class ConstructionMaterial extends Constructor{
-    public ConstructionMaterial(String contractorId, String contractorName, double materialQuantity, double materialBalance) {
-        super(contractorId, contractorName, materialQuantity, materialBalance);
-    }
-
-    public abstract void receiveMaterial();
-    public abstract void useMaterial();
-    public abstract void estimateCost();
-}
